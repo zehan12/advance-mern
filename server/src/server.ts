60be7731d06ce207d70d6f2491ccbdf6a1f00a65
Zehan Khan <zkhan@hootboard.com>
2023-09-17 15:15:32 +0530

    refactor: :recycle: handle processes in server

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,3 +11,30 @@ const server = app.listen(port, () => {
         ################################################
     `);
 });
+
+const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        logger.info("Server closed");
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  };
+  
+  const unexpectedErrorHandler = (error: unknown) => {
+    logger.error(error);
+    exitHandler();
+  };
+  
+  process.on("uncaughtException", unexpectedErrorHandler);
+  process.on("unhandledRejection", unexpectedErrorHandler);
+  
+  process.on("SIGTERM", () => {
+    logger.info("SIGTERM received");
+    if (server) {
+      server.close();
+    }
+  });
+  
